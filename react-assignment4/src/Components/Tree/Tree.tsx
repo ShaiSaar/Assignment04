8c52ed214a23c2fd6a7501e85073872e2b651907
shaisaar <shaissaar@gmail.com>
2018-06-06 16:54:53 +0300

    search

--- react-assignment4/src/Components/Tree/Tree.tsx
+++ react-assignment4/src/Components/Tree/Tree.tsx
@@ -4,9 +4,8 @@ import {events} from './common'
 import './TreeCss.css'
 
 interface ITreeState {
-    searchInput:string
+    searchInput: string
 }
-
 
 
 class Tree extends React.Component<any, ITreeState> {
@@ -23,102 +22,88 @@ class Tree extends React.Component<any, ITreeState> {
         };
         this.myRef = React.createRef();
         this.state = {
-            searchInput:""
+            searchInput: ""
         }
     }
 
 
-    searchOnTree=(input:string)=>{
+    searchOnTree = () => {
+        let input = this.state.searchInput
         const chatTree = ChatTree(this.myRef.current)
 
-        let tempArr = [
-            {type: "user",
-            name: "Shai",
-            id: 5
-            },
-            {type: "user",
-            name: "Tahel",
-            id: 5
-            },
-            {type: "user",
-            name: "Shira",
-            id: 5
-            },
-            {type: "user",
-                name: "Ohad",
-                id: 5
-            },
-            {type: "user",
-                name: "Yarden",
-                id: 5
-            },
-        ]
-        let arrResults = [];
-        for (let entry of tempArr){
-            if(entry.name.includes(input)){
-                arrResults.push(entry)
-            }
-        }
-        chatTree.load(arrResults);
-        events.on("changeOnActiveElement", this.chooseElement)
+        this.getTree()
+            .then((result: any) => {
+                if (input.length !== 0) {
+                    let arrResults = this.getSearchResults(result, input)
+                    // arrResults = [...new Set(arrResults)]
+                    chatTree.load(arrResults);
+                } else {
+                    chatTree.load(result);
+                }
 
-        // this.getTree()
-        //     .then((result: any) => {
-        //         let arrResults = this.getSearchResults(result,input)
-        //         chatTree.load(arrResults);
-        //         events.on("changeOnActiveElement", this.chooseElement)
-        //         //this.myRef.current.focus();
-        //     })
+                events.on("changeOnActiveElement", this.chooseElement)
+                //this.myRef.current.focus();
+            })
     }
 
-    getSearchResults=(tree:any,input:string)=>{
-        let arr: any = [];
+    getSearchResults = (tree: any, input: string) => {
+        let arrResults: any = [];
+        //let objResults: any = {};
 
-        function search(arr:any) {
-            for(let entry of arr){
-                if (entry.name.includes(input)){
-                    arr.push(entry)
+        function search(arr: any) {
+            for (let entry of arr) {
+                if (entry.name.toUpperCase().includes(input.toUpperCase())) {
+                    //objResults[entry.name]=entry
+                    arrResults.push(entry)
                 }
-                if(entry.type==="group"){
+                if (entry.type === "group") {
                     search(entry.items)
                 }
             }
         }
-        search(tree);
 
-        return arr;
+        search(tree);
+        // for(let entry in objResults){
+        //     arrResults.push(entry)
+        // }
+        let tmp = Array.from(arrResults, obj => JSON.stringify(obj));
+        let filtered = Array.from(Array.from(new Set(tmp)), obj => JSON.parse(obj));
+
+        return filtered;
     }
-    uploadTree=(show:boolean)=>{
+    uploadTree = (show: boolean) => {
         const chatTree = ChatTree(this.myRef.current)
         this.getTree()
             .then((result: any) => {
-                if(show){
+                if (show) {
                     chatTree.load(result);
                     events.on("changeOnActiveElement", this.chooseElement)
-                }else{
+                } else {
                     chatTree.load([]);
                 }
 
                 //this.myRef.current.focus();
             })
     }
+
     componentDidMount() {
         console.log("componentDidMount")
         this.userLogedIn = this.props.showTree;
         this.setIndex = this.props.pickedIndex
-        if(!this.props.showTree){
+        if (!this.props.showTree) {
             this.uploadTree(true)
         }
 
     }
-    componentDidUpdate(){
+
+    componentDidUpdate() {
         // console.log("Tree: this.props.showTree",this.props.showTree)
         // console.log("Tree: this.userLogedIn",this.userLogedIn)
-        if(this.userLogedIn !== this.props.showTree){
+        if (this.userLogedIn !== this.props.showTree) {
             this.userLogedIn = this.props.showTree
-            if(!this.props.showTree){
+            if (!this.props.showTree) {
                 this.uploadTree(true)
-            }else{
+            } else {
                 this.uploadTree(false)
             }
         }
@@ -139,25 +124,35 @@ class Tree extends React.Component<any, ITreeState> {
 
     };
 
-    setSearchInput=(event:any)=>{
+    setSearchInput = (event: any) => {
         this.setState({
             searchInput: event.target.value
         })
         console.log("set input", this.state.searchInput)
-        this.searchOnTree(this.state.searchInput)
+        // this.searchOnTree(this.state.searchInput)
     }
 
     public render() {
+        const searchBar = () => {
+            return (
+                <div className="Tree-searchBar">
+                    <input className="Tree-searchBar-input" type="text" onChange={this.setSearchInput} value={this.state.searchInput} placeholder="Search..."/>
+                    <i onClick={this.searchOnTree} className="fas fa-search Tree-searchBar-button"/>
+                    {/*onClick={this.searchOnTree(this.state.searchInput)}*/}
+                </div>)
+        }
         return (
 
             <section className="tree">
+                {(!this.props.showTree) ? searchBar() : null}
+
                 {/*<input type="text" onChange={this.setSearchInput} value={this.state.searchInput} placeholder="Search..."/>*/}
                 {/*<p onClick={this.props.pickedIndex.bind(null, 1)} style={style}>paragraph one</p>*/}
                 {/*<p onClick={this.props.pickedIndex.bind(null, 2)} style={style}>paragraph two</p>*/}
                 {/*<p onClick={this.props.pickedIndex.bind(null, 3)} style={style}>paragraph three</p>*/}
                 {/*<p onClick={this.props.pickedIndex.bind(null, 4)} style={style}>paragraph four</p>*/}
                 {/*<p onClick={this.getIndex} style={style}>paragraph four</p>*/}
-                <ul tabIndex={0} ref={this.myRef} />
+                <ul tabIndex={0} ref={this.myRef}/>
             </section>
 
         )
